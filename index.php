12e8a7addda01d0dc45bd45d34615b0804643923
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-12-09 09:33:02 +0700

    Add the example of looping (for, while, & do while)

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// The example of for loop
+for($i = 0; $i <5; $i++) {
+    echo "Perulangan ke-$i<br>";
+}
+
+// The example of while loop
+$i = 1;
+while($i < 15) {
+    echo "Naruto mengeluarkan kagebunshin sebanyak $i kali<br>";
+    $i++;
+}
+
+// The example of do while loop
+$i = 1;
+do {
+    echo "Sehari saya makan $i kali<br>";
+    $i++;
+} while($i <= 3);
